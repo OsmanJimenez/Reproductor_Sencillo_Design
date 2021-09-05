aa29f8c7f060700b122d51bb2052d500eb567f61
JockerCode <77410953+JockerCode@users.noreply.github.com>
2021-09-05 08:30:58 -0500

    Add Music

--- js/main.js
+++ js/main.js
@@ -5,18 +5,30 @@ class musicPlayer {
         this.playBtn.addEventListener('click', this.play);
         this.controlPanel = document.getElementById('control-panel');
         this.infoBar = document.getElementById('info');
+        this.status = false;
     }
 
     play() {
+        var audio = document.getElementById('Music'); 
         let controlPanelObj = this.controlPanel,
         infoBarObj = this.infoBar
-        Array.from(controlPanelObj.classList).find(function(element){
-            return element !== "active" ? controlPanelObj.classList.add('active') : controlPanelObj.classList.remove('active');
+        Array.from(controlPanelObj.classList).find(function(element){   
+            return element !== "active" ? controlPanelObj.classList.add('active') : controlPanelObj.classList.remove('active');       
         });
 
         Array.from(infoBarObj.classList).find(function(element){
             return element !== "active" ? infoBarObj.classList.add('active') : infoBarObj.classList.remove('active');
         });
+
+        this.status = !this.status;
+
+        if(this.status == true){
+            audio.play(); 
+        }else{
+            audio.pause();
+        }
+
+
     }
 
 }
